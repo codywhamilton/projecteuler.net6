ce44a0bf38c8d2a790a041b98a3747c8f89a4d2f
Cody W Hamilton <codywhamilton@gmail.com>
2022-01-22 12:09:26 -0500

    First Problem tests out correct (yay TDD)

--- ProjectEuler/Problem1.cs
+++ ProjectEuler/Problem1.cs
@@ -5,10 +5,17 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
+    //https://projecteuler.net/problem=1
+    //Find the sum of all the multiples of 3 or 5 below 1000. 
     public class Problem1 :Problem
     {
         public String getSolution(){
-            return "foo";
+            int result = 0;
+            for(int i=0;i<1000;i++){
+                if(i%3 == 0 || i%5 == 0)
+                    result += i;
+            }
+            return result.ToString();
         }
     }
 }
