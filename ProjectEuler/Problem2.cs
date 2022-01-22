fc81fdaa1e48450179d02de4bc8e296bbbd115f3
Cody W Hamilton <codywhamilton@gmail.com>
2022-01-22 12:13:56 -0500

    added stubs for problems 2 and 3, and failing TTD tests

---
+++ ProjectEuler/Problem2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem2:Problem
+    {
+        public String getSolution(){
+            return "foo";
+        }
+    }
+}
--- TestX/UnitTest1.cs
+++ TestX/UnitTest1.cs
@@ -10,4 +10,16 @@ public class UnitTest1
             Problem prob = new Problem1();
             Assert.Equal(prob.getSolution(),"233168");
         }
+        [Fact]
+        public void Problem2Test()
+        {
+            Problem prob = new Problem2();
+            Assert.Equal(prob.getSolution(),"4613732");
+        }
+        [Fact]
+        public void Problem3Test()
+        {
+            Problem prob = new Problem3();
+            Assert.Equal(prob.getSolution(),"6857");
+        }
 }
