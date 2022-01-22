ab844f6fdea83af877b53eeedb11c60f13445aa4
Cody W Hamilton <codywhamilton@gmail.com>
2022-01-22 14:52:46 -0500

    TDD for next 3 problems

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem5
+    {
+        String getSolution(){
+             throw new NotImplementedException("Problem not yet solved");
+         }
+    }
+}
--- TestX/UnitTest1.cs
+++ TestX/UnitTest1.cs
@@ -9,6 +9,9 @@ public class UnitTest1
     [InlineData(typeof(ProjectEuler.Problem1), "233168")]
     [InlineData(typeof(ProjectEuler.Problem2), "4613732")]
     [InlineData(typeof(ProjectEuler.Problem3), "6857")]
+    [InlineData(typeof(ProjectEuler.Problem4), "906609")]
+    [InlineData(typeof(ProjectEuler.Problem5), "232792560")]
+    [InlineData(typeof(ProjectEuler.Problem6), "25164150")]
     public void CheckEuler(Type type, string correctAnswer)
     {
                 ProjectEuler.Problem prob = (ProjectEuler.Problem) Activator.CreateInstance( type);
