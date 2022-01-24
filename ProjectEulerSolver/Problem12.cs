304e40c39f582ceddeae02326012ac694e0c4d70
Cody W Hamilton <codywhamilton@gmail.com>
2022-01-24 17:11:52 -0500

    Problem 12 test case, and solution

---
+++ ProjectEulerSolver/Problem12.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSolver
+{
+    //https://projecteuler.net/problem=12
+    //What is the value of the first triangle number to have over five hundred divisors?
+    public class Problem12 : IProblem
+    {
+        public string GetSolution()
+        {
+            int triangle = 0;
+            for(int i = 1; i < int.MaxValue; i++)
+            {
+                triangle += i; 
+                if(GetDivisorCount(triangle) > 500){
+                    return triangle.ToString();
+                }
+            }
+
+            return "";
+        }
+        private long GetDivisorCount(long number)
+        {
+            //will not be 1 for this problem but for completness of this function if I reuse the code elsewhere.
+            if (number == 1)
+                return 1;
+
+            int count = 0;
+            int squareRoot = (int) Math.Ceiling(Math.Sqrt(number));
+            //if x is a divisor of number then number/x is also a divisor. We only need to find the divisors upto the cealing of square root of the number
+            for(int i = 1; i <= squareRoot; i++){
+                if(number % i == 0){
+                    count++; 
+                }
+            }
+            return count * 2;
+        }
+        
+    }
+}
--- TestX/UnitTest1.cs
+++ TestX/UnitTest1.cs
@@ -17,6 +17,7 @@ public class UnitTest1
     [InlineData(typeof(ProjectEulerSolver.Problem9), "31875000")]
     [InlineData(typeof(ProjectEulerSolver.Problem10), "142913828922")]
     [InlineData(typeof(ProjectEulerSolver.Problem11), "70600674")]
+    [InlineData(typeof(ProjectEulerSolver.Problem12), "76576500")]
     public void CheckEuler(Type type, string correctAnswer)
     {
         ProjectEulerSolver.IProblem prob = (ProjectEulerSolver.IProblem)Activator.CreateInstance(type);
