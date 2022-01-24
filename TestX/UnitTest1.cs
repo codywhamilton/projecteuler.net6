679e13a99eb53f70b0e3eff58297a61d75a0db74
Cody W Hamilton <codywhamilton@gmail.com>
2022-01-24 17:30:04 -0500

    Test case and solution for problem 13

--- TestX/UnitTest1.cs
+++ TestX/UnitTest1.cs
@@ -18,6 +18,7 @@ public class UnitTest1
     [InlineData(typeof(ProjectEulerSolver.Problem10), "142913828922")]
     [InlineData(typeof(ProjectEulerSolver.Problem11), "70600674")]
     [InlineData(typeof(ProjectEulerSolver.Problem12), "76576500")]
+    [InlineData(typeof(ProjectEulerSolver.Problem13), "5537376230")]
     public void CheckEuler(Type type, string correctAnswer)
     {
         ProjectEulerSolver.IProblem prob = (ProjectEulerSolver.IProblem)Activator.CreateInstance(type);
